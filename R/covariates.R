3ca65674ff27730a871ef52b4f77f4b4ee3bb1b2
Your Name <momalikhd@hotmail.com>
2024-10-21 17:42:47 -0400

    Initial commit

---
+++ R/covariates.R
@@ -0,0 +1,27 @@
+#' Example dataframe
+#'
+#' Sample data with multiple variables with names of varying lengths, spacing,
+#' and captilization. This dataframe also contains variables with similar names
+#' which the function deals with by adding col# to the end of repeated named
+#' variables
+#'
+#'
+#' @docType data
+#' @format A data frame with 3720 rows and 13 variables: {
+#' variable names:
+#' country_name
+#' ISO
+#' region
+#' year
+#' GDP1000
+#' OECD
+#' OECD2023
+#' popdens
+#' urban
+#' agedep
+#' male_edu
+#' temp
+#' rainfall1000
+#'
+#' }
+"covariates"
