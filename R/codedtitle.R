470acf2a50465a8038405c40fc227bea4529c5fb
Your Name <momalikhd@hotmail.com>
2024-10-28 12:55:40 -0400

    update

--- R/codedtitle.R
+++ R/codedtitle.R
@@ -1,7 +1,9 @@
 #' @title Code Variable Names of Dataframe
 #' @description Take a dataset and change column names to manageable
 #' coded variables names based on a desired string-length. This function also
-#' removes all spaces and makes all characters lower-case.
+#' removes all spaces and makes all characters lower-case. A reference vector is
+#' created (coderef) which display the transformed column names and the original
+#' column name.
 #' @param dataframe with named columns
 #' @return dataframe with recoded column names
 #' @author Mohsyn Imran Malik
@@ -12,6 +14,7 @@
 codevar <- function(data, strlength) {
   collength <- nchar(colnames(data))
   namescol <- colnames(data)
+  coderef <- character(length(collength))
   for (i in 1:(length(collength))) {
     if (collength[i] > strlength) {
       namescol[i] <- gsub(" ", "",
@@ -23,12 +26,13 @@ codevar <- function(data, strlength) {
                                  tolower(namescol[i]))
 
     }
-    if (namescol[i] %in% namescol[length(collength)-i] == TRUE) {
+    if (namescol[i] %in% namescol[1:i-1] == TRUE) {
         namescol[i] <- gsub(" ","",paste(namescol[i],i))
-       }
+    }
+    coderef[i] <- paste(colnames(data)[i], "=", namescol[i])
   }
-colnames(data) <- namescol
-print(colnames(data))
+  assign("coderef", coderef, .GlobalEnv)
+print(namescol)
 
 }
 
